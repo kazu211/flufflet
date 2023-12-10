d1c27e7545e450f30e732287ec24f98d01de4ee3
kazu211 <123786218+kazu211@users.noreply.github.com>
2023-12-10 15:40:20 +0900

    Add header

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar scroll-behavior="hide" color="primary">
+      <v-toolbar-title class="title">Flufflet</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="navigateTo('/')" icon="mdi-table"></v-btn>
+      <v-btn @click="navigateTo('/settings')" icon="mdi-cog"></v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+
+</v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo(path) {
+      this.$router.push(path);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  color: #38302E;
+  font-size: 30px;
+  font-family: cursive;
+  font-weight: bold;
+}
+</style>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,13 +1,32 @@
-import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#F8FFE5',
+    surface: '#FFFFFF',
+    primary: '#FFC43D',
+    secondary: '#EFEA5A',
+    error: '#EF476A',
+    info: '#38302E',
+    success: '#06D6A0',
+  },
+}
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: true,
     components,
     directives,
-    // 他の設定をここに記述していく
+    theme: {
+      defaultTheme: 'myCustomLightTheme',
+      themes: {
+        myCustomLightTheme,
+      },
+    },
   })
 
   // Vue.js で Vuetify を使用する
