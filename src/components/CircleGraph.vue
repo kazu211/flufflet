ebacfb0e4a4f6b91a3519c5b544d09f24c2b6b5c
kazu211 <123786218+kazu211@users.noreply.github.com>
2024-08-18 12:03:09 +0900

    グラフを出せるようにした

---
+++ src/components/CircleGraph.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { ChartData, ChartOptions } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Title, Tooltip, Legend)
+
+const data:ChartData<'doughnut'> = {
+  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [20, 40, 50, 10]
+    }
+  ]
+}
+
+const options:ChartOptions<'doughnut'> = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+</script>
+
+<template>
+  <Doughnut :data="data" :options="options"/>
+</template>
